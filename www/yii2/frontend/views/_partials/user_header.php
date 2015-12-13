039359146202be9ea72df714f29f1f6011bbb4e3
denikewebpost@yandex.ua <den@lux-blog.org>
2015-12-13 07:52:39 +0200

    landing

--- www/yii2/frontend/views/_partials/user_header.php
+++ www/yii2/frontend/views/_partials/user_header.php
@@ -1,1 +1,13 @@
-user_header
+<div class="landing header">
+	<div class="header-image">
+
+	</div>
+	<div class="header-blackout">
+
+	</div>
+	<div class="header-text">
+		Epulari — знайди хату на свято
+		<div><a href="/users/signup" class="btn btn-lg btn-success ep-dom-sign-up-pass">Зареєструватись</a></div>
+	</div>
+
+</div>
